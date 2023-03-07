93543f744b322e00b6430c74d37ecf0e9964aefe
LinlyBoi <libkyy@e.email>
2023-03-07 18:15:15 +0200

    setSize is not setPreferredSize

--- src/main/java/org/bootcamp/TrafficLights/Sign.java
+++ src/main/java/org/bootcamp/TrafficLights/Sign.java
@@ -14,7 +14,7 @@ public class Sign extends JFrame {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLayout(new GridLayout(4, 1));
     this.setPreferredSize(new Dimension(600, 600));
-    this.setSize(600, 600);
+    this.setSize(175, 600);
     this.setLocationRelativeTo(null);
     this.setVisible(true);
     this.getContentPane().setBackground(Color.BLACK);
